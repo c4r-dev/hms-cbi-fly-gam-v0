785d4c3646bebb658daa528cfb8b36fed091e2dc
David Fischer <fiscd@seas.upenn.edu>
2025-01-07 12:48:35 -0500

    commit

--- app/obstaclelist/page.js
+++ app/obstaclelist/page.js
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
 import ObstacleListClient from "./ObstacleListClient"; // Separate client-side logic into a component
-import "./globals.css"; // Ensure global styles are loaded
 
 export default function ObstacleListPage() {
   return (
