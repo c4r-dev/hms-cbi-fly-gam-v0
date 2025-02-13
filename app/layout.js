c81c7da38384ef42ea95714adfdf405d2f8985c3
Jack Liddy <36009149+JackLiddy@users.noreply.github.com>
2025-02-13 13:50:55 -0500

    test google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,28 @@
+import Script from 'next/script';
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+                   {/* Google Analytics Script */}
+                   <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-H8WQHE6FS4"
+            strategy="afterInteractive"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-H8WQHE6FS4');
+              `,
+            }}
+          />
         <div>{children}</div>
       </body>
     </html>
